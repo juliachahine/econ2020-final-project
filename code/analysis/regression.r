0588db3c2395edf654f6d437529181ca62aef0f7
juliachahine <jrchahine@gmail.com>
2024-04-16 21:55:26 -0400

    updated regression

--- code/analysis/regression.r
+++ code/analysis/regression.r
@@ -3,7 +3,11 @@ source("code/data-management/flights-airfare-merge.r")
 library(estimatr)
 library(fixest)
 
-mod4 <- feols(fare_pm ~ as.factor(CARRIER) | ORIGIN + DEST| HUB2HUB ~  AS_HUB2HUB + AA_HUB2HUB + DL_HUB2HUB + G4_HUB2HUB + F9_HUB2HUB + NK_HUB2HUB + WN_HUB2HUB + UA_HUB2HUB, flight_prices)
+hub_iv_mod <- feols(fare_pm ~ as.factor(CARRIER)| ORIGIN + YEAR + DEST| HUB2HUB ~  AS_HUB2HUB + AA_HUB2HUB + DL_HUB2HUB + G4_HUB2HUB + F9_HUB2HUB + NK_HUB2HUB + WN_HUB2HUB + UA_HUB2HUB, flight_prices)
 
-summary(mod4)
+hub_iv_mod <- feols(fare_pm ~ as.factor(CARRIER)| ORIGIN + YEAR + DEST + fare_lg| HUB2HUB ~  AS_HUB2HUB + AA_HUB2HUB + DL_HUB2HUB + G4_HUB2HUB + F9_HUB2HUB + NK_HUB2HUB + WN_HUB2HUB + UA_HUB2HUB, flight_prices)
 
+
+# etable(hub_iv_mod,tex = TRUE)
+summary(hub_iv_mod)
+etable(hub_iv_mod)
